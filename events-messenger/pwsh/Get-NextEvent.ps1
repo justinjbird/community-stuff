bb20fdf17909cc00feb1f448a2fdd4e4c87769b6
Justin Bird <justin@metricy.uk>
2023-08-04 23:56:18 +0100

    stripped back message

--- events-messenger/pwsh/Get-NextEvent.ps1
+++ events-messenger/pwsh/Get-NextEvent.ps1
@@ -6,12 +6,11 @@ param(
 $events = Import-Csv -Path $path
 $next = ($events | Select-Object time -Unique -First 1).time
 $nextEvents = ($events | Where-Object { $_.time -eq $next })
-$nextEventsMessage = @()
-$nextEventsMessage += "Coming up next...\n\n"
+$message = @()
+$message += "Coming up next...\n\n"
 if ($nextEvents) {
   foreach ($event in $nextEvents) {
-    $nextEventsMessage += "$($event.name) with $($event.talk) in the $($event.room) room\n\n"
+    $message += "$($event.name) with $($event.talk) in the $($event.room) room\n\n"
   }
 }
-$message = "{""text"":""${nextEventsMessage}""}"
 Write-Output "message=$message" >> $env:GITHUB_OUTPUT
